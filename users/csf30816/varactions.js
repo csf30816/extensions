65aeabc1887a35590056b8fb97d30f3efef0c66d
csf30816 <elijahporter@zoho.com>
2016-07-17 18:15:24 -0500

    Update varactions.js

--- users/csf30816/varactions.js
+++ users/csf30816/varactions.js
@@ -1,1 +1,42 @@
+(function(ext) {
+    ext._shutdown = function() {};
 
+    ext._getStatus = function() {
+        return {
+            status: 2,
+            msg: 'Ready to use :)'
+        }
+    };
+    var vars = {
+};
+
+    var descriptor = {
+        blocks: [
+	    [' ', 'Create var %s', 'create', 'var'],
+	    [' ', 'Set Var %s to %s', 'set', 'var', '0'],
+	    [' ', 'Change %s by %n', 'change', '', 1],
+	    ['r', 'Read Var %s', 'read', ''],
+	    ['r', 'Who made this?', 'author']
+        ],
+
+	menus: {
+	}
+    };
+    ext.set = function(v, value) {
+	vars[v] = value;
+    };
+    ext.create = function(v) 
+    { varHold[v] = "0"
+    };
+    ext.change = function(v, value) {
+	vars[v] += value;
+    };
+    ext.read = function(v) {
+	return vars[v];
+    };
+    ext.author = function(a) {
+	return '@Csf30816 made this project :)';
+    };
+
+    ScratchExtensions.register('Variable Scripts', descriptor, ext);
+})({});
