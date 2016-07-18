ebbf2a54349d9ed41f9887ac1a3912d49a3c9d3c
csf30816 <elijahporter@zoho.com>
2016-07-18 14:40:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ var strategy = require('./setup-passport');
 // Session and cookies middlewares to keep user logged in
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+
+app.use(cookieParser());
+// See express session docs for information on the options: https://github.com/expressjs/session
+app.use(session({ secret: 'E88bztDtzMYbBh1XQpFwddW3w_xjpN6pgFgx7P5wc8jQU-Z8i-dYvGlezVCjT_t7', resave: false,  saveUninitialized: false }));
+...
+app.use(passport.initialize());
+app.use(passport.session());
+...
