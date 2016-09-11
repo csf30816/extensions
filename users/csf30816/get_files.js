4f0c54a5b16c0b74fe82680a4507dcfc3fdf79ee
csf30816 <elijahporter@zoho.com>
2016-09-11 15:33:46 -0500

    Update get_files.js

--- users/csf30816/get_files.js
+++ users/csf30816/get_files.js
@@ -1,1 +1,26 @@
+(function(ext) {
+    ext._shutdown = function() {};
 
+    ext._getStatus = function() {
+        return {
+            status: 2,
+            msg: 'Ready to use :)'
+        };
+    };
+
+    var descriptor = {
+        blocks: [
+            [' ', 'Get file from URL: %s', 'getURL', 'https://scratch.mit.edu']
+        ],
+
+        menus: {}
+    };
+    ext.getURL = function(url) {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open("GET", url, false);
+        xmlHttp.send();
+        alert(xmlHttp.responseText);
+    };
+
+    ScratchExtensions.register('Get File', descriptor, ext);
+})({});
