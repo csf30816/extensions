e202510cb82a490ce3913d03edd23193fb976096
csf30816 <elijahporter@zoho.com>
2016-09-11 16:11:56 -0500

    Update and rename get_files.js to api.js

---
+++ users/csf30816/api.js
@@ -0,0 +1,39 @@
+(function(ext) {
+    ext._shutdown = function() {};
+
+    ext._getStatus = function() {
+        return {
+            status: 2,
+            msg: 'Ready to use :)'
+        };
+    };
+
+    var descriptor = {
+        blocks: [
+            [' ', 'Follow %s', 'follow', 'csf30816'],
+            ['', '%s s messages number', 'messages', 'csf30816']
+        ],
+
+        menus: {}
+    };
+    ext.follow = function (user) {
+        $.ajax({
+            type: "PUT",
+            url: "https://scratch.mit.edu/site-api/users/followers/" + user + "/add/",
+            data: {
+                usernames: Scratch.INIT_DATA.LOGGED_IN_USER.model.username
+            }
+        })
+    }
+    ext.messages = function (callback) {
+        $.ajax({
+            url: 'https://scratch.mit.edu/messages/ajax/get-message-count/',
+            dataType: 'json',
+            success: function (responseText) {
+                number = responseText['msg_count'];
+                return(number);
+            }
+        })
+    }
+    ScratchExtensions.register('Scratch API', descriptor, ext);
+})({});
