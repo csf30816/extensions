3948be7ac3896d0b300d1efe142808a69ea0b890
csf30816 <elijahporter@zoho.com>
2016-10-16 13:26:23 -0500

    Update bank-extensions.js

--- users/csf30816/bank-extensions.js
+++ users/csf30816/bank-extensions.js
@@ -1,1 +1,25 @@
+(function(ext) {
+    ext._shutdown = function() {};
 
+    ext._getStatus = function() {
+        return {
+            status: 2,
+            msg: 'Ready to use :)'
+        };
+    };
+    var vars = {};
+
+    var descriptor = {
+        blocks: [
+            [' ', 'Create var %s', 'create', 'var'],
+            [' ', 'Set Var %s to %s', 'set', 'var', '0'],
+            [' ', 'Change %s by %n', 'change', 'var', 1],
+            ['r', 'Read Var %s', 'read', 'var'],
+            ['r', 'Who made this?', 'author']
+        ],
+
+        menus: {}
+    };
+
+    ScratchExtensions.register('Variable Scripts', descriptor, ext);
+})({});
